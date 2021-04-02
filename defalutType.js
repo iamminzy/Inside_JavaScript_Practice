438549fc1380c2b79063dd3965419deb0ff7c00b
iamminzy <iamminzy92@gmail.com>
2021-04-02 12:27:15 +0900

    ex 3.1

---
+++ defalutType.js
@@ -0,0 +1,15 @@
+var intNum      = 10;
+var floatNum    = 0.1;
+var singleChar  = 'a';
+var boolVar     = true;
+var emptyVar;
+var nullVar     = null;
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleChar,
+    typeof boolVar,
+    typeof emptyVar,
+    typeof nullVar
+)
