c44d3901b878cfbe71497bb8c4a96cc2f581d675
iamminzy <iamminzy92@gmail.com>
2021-04-02 12:50:39 +0900

    practice

--- defalutType.js
+++ defalutType.js
@@ -13,4 +13,10 @@ console.log(
     typeof emptyVar,
     typeof nullVar
 )
-//출력 결과 : number number string boolean undefined object
+//출력 : number number string boolean undefined object
+
+
+/* 
+자바스크립트는 변수를 선언할 때 타입을 미리 정하지 않고,
+var 라는 키워드로만 변수를 선언한다.
+*/
---
+++ string.js
@@ -0,0 +1,12 @@
+// str 문자열 생성
+var str = 'test';
+console.log(str[0], str[1], str[2], str[3]);
+// 출력 : test
+
+//문자열의 첫 글자를 대문자로 변경?
+str[0] = 'T';
+console.log(str);
+//출력 : test
+
+
+/* 한번 정의된 문자열은 변하지 않는다. */
