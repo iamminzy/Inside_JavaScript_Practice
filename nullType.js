8946c1e820b56db0d2b41855219d1de412a5c58d
iamminzy <iamminzy92@gmail.com>
2021-04-02 13:36:02 +0900

    nullType

---
+++ nullType.js
@@ -0,0 +1,12 @@
+//null 타입 변수 체크
+var nullVar = null;
+
+console.log(typeof nullVar === null);
+console.log(nullVar === null);
+//출력 : false true
+
+
+/*
+null 타입의 변수는 값은 null이지만 타입은 null이 아니라
+object이다.
+*/
